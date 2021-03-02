1476e20b544f7bc07443d9ff88b060da6662b60f
Marc <marc.ferraro1@gmail.com>
2021-03-02 11:21:15 -0600

    Toy form event handlers

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
 class ToyForm extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name: "",
+      imageUrl: "",
+    }
+  }
+
+  handleName = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+    console.log(this.state.name)
+  }
+
+  handleUrl = (event) => {
+    this.setState({
+      imageUrl: event.target.value
+    })
+    console.log(this.state.imageUrl)
+
+  }
 
   render() {
     return (
       <div className="container">
         <form className="add-toy-form">
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input onChange={this.handleName} value={this.state.name} type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input onChange={this.handleUrl} value={this.state.imageUrl} type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
